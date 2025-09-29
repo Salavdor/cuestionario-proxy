edc4d6def5d8a070d5b74d6850d4fc6829e926fe
Salavdor <salvador.stereo@gmail.com>
2025-09-29 13:17:40 -0600

    desactivar cors para test

--- api/feedback.js
+++ api/feedback.js
@@ -7,13 +7,13 @@ export default async function handler(req, res) {
   // res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // <--- Asegura el encabezado
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  // if (req.method === 'OPTIONS') {
+  //   res.setHeader('Access-Control-Allow-Origin', '*'); // <--- Asegura el encabezado
+  //   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    return res.status(200).end();
-  }
+  //   return res.status(200).end();
+  // }
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método no permitido" });
